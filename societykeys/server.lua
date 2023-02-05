8c654c0f1017228468893be6a3ab0fccaeedb555
Snov <snov@snov.dev>
2023-02-05 14:53:30 +0100

    change ESX import method

--- societykeys/server.lua
+++ societykeys/server.lua
@@ -1,22 +1,13 @@
-ESX = nil
+ESX = exports['es_extended']:getSharedObject()
 
-Citizen.CreateThread(function()
-    while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(0)
-	end
-
-    ESX.RegisterServerCallback('betterpd:getFactionVehiclePlates', function(src, cb, ownerString)
-        MySQL.Async.fetchAll('SELECT plate FROM owned_vehicles WHERE owner = @owner', {
-            ['@owner'] = ownerString,
-        }, function(result)
-            ret = {}
-            for i, v in ipairs(result) do
-                ret[i] = v
-            end
-            cb(ret)
-        end)
+ESX.RegisterServerCallback('betterpd:getFactionVehiclePlates', function(src, cb, ownerString)
+    MySQL.Async.fetchAll('SELECT plate FROM owned_vehicles WHERE owner = @owner', {
+        ['@owner'] = ownerString,
+    }, function(result)
+        ret = {}
+        for i, v in ipairs(result) do
+            ret[i] = v
+        end
+        cb(ret)
     end)
 end)
-
-
